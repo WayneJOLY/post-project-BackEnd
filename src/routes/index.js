2ed19fe048319b6ed4a29fede76295381b8894f0
Gerald <geraldjoly191@gmail.com>
2024-11-28 08:26:48 -0300

    the back end is complete

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const routerPost = require('./post.router');
 const router = express.Router();
 
 // colocar las rutas aquí
-
+router.use("/posts",routerPost)
 
 module.exports = router;
